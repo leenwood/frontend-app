698a15a380a9b4b603cdc34d4f5760251496d5f2
Irina946 <Irina.Larina@urfu.me>
2023-06-13 16:16:36 +0500

    Сделан список всех предметов

--- src/pages/all-subject/all-sublect.js
+++ src/pages/all-subject/all-sublect.js
@@ -1,20 +1,17 @@
 import Subject from "../ui-elements/one-subject/one-subject";
 import Styles from "./allSubjects.module.css"
 import { NUMBER_SEMESTER } from "./subjects.data";
+import taskData from "./description.json";
 
 function Subjects() {
+    const arOptions = taskData.description_courses;
+    const listCourses = arOptions.map((arOption, index) => 
+    <Subject subject_name={arOption.subject_name} subject_type={arOption.subject_type} title_subject={arOption.title_subject} teatcher_name={arOption.teatcher_name} time_count={arOption.time_count} author_courses={arOption.author_courses} />)
     return <div className={Styles.all_subject}><div className={Styles.container__subjects}>
         {NUMBER_SEMESTER.map((number) => (
             <div className={Styles.row_subjects}>
                 <h1 className={Styles.number_semester}>{number}</h1>
-                <Subject subject_name='Философия' subject_type='humanitarian' title_subject="Философия. Курс для начинающих." teatcher_name="Иванов И. И." time_count="30 часов" author_courses="Курс разработан ИтМО" />
-                <Subject subject_name='Векторный анализ' subject_type='math' title_subject="Основы векторного анализа" teatcher_name="Иванов И. И." time_count="30 часов" author_courses="Курс разработан ИтМО" />
-                <Subject subject_name='Основы С++' subject_type='programming' title_subject="Основы применения языка программирования С++ " teatcher_name="Иванов И. И." time_count="30 часов" author_courses="Курс разработан ИтМО" />
-                <Subject subject_name='Цифровая медицина' subject_type='humanitarian' title_subject="Цифровая медицина. Реалии XXI века." teatcher_name="Иванов И. И." time_count="30 часов" author_courses="Курс разработан ИтМО" />
-                <Subject subject_name='Бизнес-анализ' subject_type='analytical' title_subject="Бизнес-анализ в масштабируемых системах" teatcher_name="Иванов И. И." time_count="30 часов" author_courses="Курс разработан ИтМО" />
-                <Subject subject_name='Физика' subject_type='natural' title_subject="Физика магических взаимодействий" teatcher_name="Иванов И. И." time_count="30 часов" author_courses="Курс разработан ИтМО" />
-                <Subject subject_name='Микроконтроллеры AVR' subject_type='programming' title_subject="Программирование микроконтроллеров AVR" teatcher_name="Иванов И. И." time_count="30 часов" author_courses="Курс разработан ИтМО" />
-                <Subject subject_name='Ителлектуальный анализ данных' subject_type='analytical' title_subject="Интеллектуальный анализ данных" teatcher_name="Иванов И. И." time_count="30 часов" author_courses="Курс разработан ИтМО" />
+                {listCourses}
             </div>
         ))}
     </div>
